17889e9461392e5a3a4cfdb38490cddc549dbb42
paullaster <paullasterokoth98@gmail.com>
2023-01-12 22:52:17 +0300

    commit files

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,27 @@
 import LabeledInput from "./LabeledInput";
 
-const TodoList = ({todos}) =>{
-    return(
-        <ul>
-            {
-                todos.map(({id, ...todo}) =>{
-                    return(
-                        <TodoListItem key={id} {...todo}/>
-                    )
-                })
-            }
-        </ul>
-    );
+const TodoList = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(({ id, ...todo }) => {
+        return <TodoListItem key={id} {...todo} />;
+      })}
+    </ul>
+  );
 };
 
-const TodoListItem = ({label, created_at}) =>{
-    return(
-        <LabeledInput
-            type="checkbox"
-            ></LabeledInput>
-    )
+const TodoListItem = ({ label, created_at }) => {
+  return (
+  <LabeledInput type="checkbox" >
+    <li>
+                <span>
+                    {label}
+                </span>
+            <span>
+                {created_at}
+            </span>
+        </li>
+  </LabeledInput>
+  );
 };
 export default TodoList;
