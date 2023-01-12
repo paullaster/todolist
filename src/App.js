f0970020b2a56523bb376df2520080a0df240091
paullaster <paullasterokoth98@gmail.com>
2023-01-12 23:27:08 +0300

    commit files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import CreateToDo from "./components/CreateToDo";
