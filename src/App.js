13582a9b4c12557ac2a5d7ce98bd7b395858555f
paullaster <paullasterokoth98@gmail.com>
2023-01-12 21:11:31 +0300

    commit files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import React, {useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
-import './App.css';
-import CreateToDo from './components/CreateToDo';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import CreateToDo from "./components/CreateToDo";
 
 const App = () => {
-  const [todos, setTodos] = useState (
-    JSON.parse(localStorage.getItem('todos')) || []
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
   );
-  const [task, setTasks] = useState ([]);
+  const [task, setTasks] = useState([]);
 
-const onChangeHandler = (e) => {
-  setTasks ( e.target.value);
-};
-const onClickHandler = (e) => {
-  e.preventDefault();
-  const newTask = {
-    id: uuidv4(),
-    labale: task,
-    completed: false,
+  const onChangeHandler = e => {
+    setTasks(e.target.value);
   };
-  console.log (task, newTask)
-  
-  setTodos (
-    (prev) => {
-      return prev = [...prev, {
-        
-      }]
-    }
-  );
-  // localStorage.setItem('todos', JSON.stringify(todos));
-  // console.log (todos)
-  
-};
-  console.log (todos)
+  const onClickHandler = e => {
+    e.preventDefault();
+    const newTask = {
+      id: uuidv4(),
+      labale: task,
+      completed: false
+    };
+    console.log(task, newTask);
+
+    setTodos(prev => {
+      return (prev = [
+        ...prev,
+        {
+          id: uuidv4(),
+          labale: task,
+          completed: false
+        }
+      ]);
+    });
+    // localStorage.setItem('todos', JSON.stringify(todos));
+    // console.log (todos)
+  };
+  console.log(todos);
   return (
-    <CreateToDo 
-    onClickHandler={onClickHandler}
-    onChangeHandler={onChangeHandler}
+    <CreateToDo
+      onClickHandler={onClickHandler}
+      onChangeHandler={onChangeHandler}
     />
   );
-}
+};
 
 export default App;
