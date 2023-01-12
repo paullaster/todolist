e90c3cc7651155ead7b35b7dd13a27bbef5db87e
paullaster <paullasterokoth98@gmail.com>
2023-01-12 16:45:43 +0300

    commit files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {uuidv4} from 'uuidv4';
 import './App.css';
 import CreateToDo from './components/CreateToDo';
 
@@ -11,7 +12,9 @@ const onChangeHandler = (e) => {
   setTodos(
     (prev) => {
       return prev = [...prev, {
-        name: e.target.value.toLowerCase
+        id: uuidv4(),
+        name: e.target.value.toLowerCase,
+        checked: false,
       }]
     }
   );
